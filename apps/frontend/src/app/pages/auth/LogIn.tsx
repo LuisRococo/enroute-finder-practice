1f2eff97c8a66f3e1e3fc486b94a6a30bb59a49b
Luis Roberto <luisroberto@Luiss-MacBook-Pro-2.local>
2022-06-07 21:41:44 -0700

    Added formik to login form

--- apps/frontend/src/app/pages/auth/LogIn.tsx
+++ apps/frontend/src/app/pages/auth/LogIn.tsx
@@ -2,29 +2,53 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AuthCont from '../../components/auth/AuthCont';
 import routes from '../../../util/routes';
+import { useFormik } from 'formik';
 
 function LogIn() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: onFormSubmit,
+  });
+
+  function onFormSubmit(values: any) {
+    const { username, password } = values;
+    alert('submit');
+  }
+
   return (
     <>
-      <AuthCont title="Log In To start meeting people!">
-        <input
-          className="auth-card__input"
-          type="text"
-          placeholder="Username"
-        />
+      <form onSubmit={formik.handleSubmit}>
+        <AuthCont title="Log In To start meeting people!">
+          <input
+            className="auth-card__input"
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+          />
 
-        <input
-          className="auth-card__input"
-          type="text"
-          placeholder="Password"
-        />
+          <input
+            className="auth-card__input"
+            type="text"
+            placeholder="Password"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
 
-        <button className="btn">Submit</button>
+          <button type="submit" className="btn">
+            Submit
+          </button>
 
-        <Link className="autn-card__link" to={routes.signup.url}>
-          Sign Up
-        </Link>
-      </AuthCont>
+          <Link className="autn-card__link" to={routes.signup.url}>
+            Sign Up
+          </Link>
+        </AuthCont>
+      </form>
     </>
   );
 }
