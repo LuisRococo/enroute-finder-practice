c1e48330e6b048a551611a7651e8a8083bd815ce
Luis Roberto <luisroberto@Luiss-MacBook-Pro-2.local>
2022-06-10 12:47:05 -0700

    Added personal questions definition to index

--- libs/definitions/src/index.ts
+++ libs/definitions/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/chat/index';
 export * from './lib/loveHistory/index';
 export * from './lib/match/index';
 export * from './lib/user/index';
+export * from './lib/personalQuestions/index';
