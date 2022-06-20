0cde857406f63cc63dd2bfb7bdfd9eb0e03e6740
Luis Roberto <luisrobertopenav@gmail.com>
2022-06-19 21:12:03 -0700

    Refactored look and feel

--- apps/main-front/src/app/components/home/other-info/data.ts
+++ apps/main-front/src/app/components/home/other-info/data.ts
@@ -3,9 +3,9 @@ import { GiChainedHeart, GiLockedHeart } from 'react-icons/gi';
 import { SmallCardInterface } from './SmallCard';
 
 export const items: SmallCardInterface[] = [
-   { Icon: BsEmojiHeartEyesFill, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#e33d5f' },
-   { Icon: BsHeartFill, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#ffa6e9' },
-   { Icon: BsBookmarkHeartFill, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#e33d5f' },
-   { Icon: GiLockedHeart, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#ffa6e9' },
-   { Icon: GiChainedHeart, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#e33d5f' },
+   { Icon: BsEmojiHeartEyesFill, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#962BD4' },
+   { Icon: BsHeartFill, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#AB55DD' },
+   { Icon: BsBookmarkHeartFill, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#962BD4' },
+   { Icon: GiLockedHeart, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#AB55DD' },
+   { Icon: GiChainedHeart, info: 'Find Dates easy with our algorithm', title: 'Easy to use', iconColor: '#962BD4' },
 ];
