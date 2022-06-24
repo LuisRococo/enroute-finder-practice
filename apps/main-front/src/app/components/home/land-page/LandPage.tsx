105010608faf26705a253aeb8786ace69c8408cb
Luis Roberto <luisrobertopenav@gmail.com>
2022-06-23 18:52:07 -0700

    Small refactorings

--- apps/main-front/src/app/components/home/land-page/LandPage.tsx
+++ apps/main-front/src/app/components/home/land-page/LandPage.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from 'react';
 import './styles.scss';
 import { Button as ButtonFinder } from '@finder/components';
+import { Link } from 'react-router-dom';
+import routes from 'apps/main-front/src/util/routes';
 
 const LandPage: FC = () => {
    return (
       <div className="land-pg wrapper over-ride">
          <h3 className="land-pg__title">Find your new friends on Finder !</h3>
          <p className="land-pg__sub-title">Great people awaits you on this website</p>
-         <ButtonFinder className="override btn" text="Explore" />
+         <Link to={routes.find.url}>
+            <ButtonFinder className="override btn" text="Explore" />
+         </Link>
       </div>
    );
 };
